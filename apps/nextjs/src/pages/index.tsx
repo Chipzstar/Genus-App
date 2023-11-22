0ae779f8054de10878620b8ef045ffccde396edd
Chipzstar <chipzstar.dev@googlemail.com>
2023-11-22 20:42:12 +0000

    add custom form schema for login

--- apps/nextjs/src/pages/index.tsx
+++ apps/nextjs/src/pages/index.tsx
@@ -1,68 +1,40 @@
 import type {NextPage} from 'next';
 import Head from 'next/head';
 import {trpc} from '../utils/trpc';
-import { zodResolver } from "@hookform/resolvers/zod"
-import type {inferProcedureOutput} from '@trpc/server';
-import type {AppRouter} from '@genus/api';
+import {zodResolver} from "@hookform/resolvers/zod"
 import {useAuth, UserButton} from '@clerk/nextjs';
 import Link from 'next/link';
 import Image from 'next/image';
-import { Button } from "@genus/ui/button";
-import { Form, FormItem, FormLabel, FormControl, FormDescription, FormMessage, FormField } from "@genus/ui/form";
-import { Input } from "@genus/ui/input";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle} from "@genus/ui/card";
+import {Button} from "@genus/ui/button";
+import {Form, FormControl, FormField, FormItem, FormMessage} from "@genus/ui/form";
+import {Input} from "@genus/ui/input";
 import {useForm} from 'react-hook-form';
-import { z } from 'zod';
-import { BellRing, Check } from "lucide-react"
-import {Switch} from "@genus/ui/switch";
-import { cn } from "@genus/ui";
-
-/*const PostCard: React.FC<{
-    post: inferProcedureOutput<AppRouter['post']['all']>[number];
-}> = ({post}) => {
-    return (
-        <div className='max-w-2xl rounded-lg border-2 border-gray-500 p-4 transition-all hover:scale-[101%]'>
-            <h2 className='text-2xl font-bold text-[hsl(280,100%,70%)]'>{post.title}</h2>
-            <p>{post.content}</p>
-        </div>
-    );
-};*/
-
-const notifications = [
-    {
-        title: "Your call has been confirmed.",
-        description: "1 hour ago",
-    },
-    {
-        title: "You have a new message!",
-        description: "1 hour ago",
-    },
-    {
-        title: "Your subscription is expiring soon!",
-        description: "2 hours ago",
-    },
-]
+import {z} from 'zod';
+import {signupSchema} from "~/schemas";
 
 const Home: NextPage = () => {
     const postQuery = trpc.post.all.useQuery();
 
-    const formSchema = z.object({
-        username: z.string().min(2, {
-            message: "Username must be at least 2 characters.",
-        }),
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof signupSchema>>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+            confirmPassword: '',
+            gender: 'male',
+            completion_year: '',
+            university: '',
+            degree_course: '',
+            career_interests: ''
+        },
+        resolver: zodResolver(signupSchema),
     })
 
-    // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
-        defaultValues: {
-            username: "",
-        },
-    })
-    function onSubmit(values: z.infer<typeof formSchema>) {
+    function onSubmit(values: z.infer<typeof signupSchema>) {
         // Do something with the form values.
         // ✅ This will be type-safe and validated.
-        console.log(values)
+        alert(JSON.stringify(values, null, 2))
     }
 
     return (
@@ -83,68 +55,43 @@ const Home: NextPage = () => {
                             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
                                 <FormField
                                     control={form.control}
-                                    name="username"
-                                    render={({ field }) => (
+                                    name="name"
+                                    render={({field}) => (
                                         <FormItem>
-                                            <FormLabel>Username</FormLabel>
                                             <FormControl>
-                                                <Input placeholder="shadcn" {...field} />
+                                                <Input placeholder="Name" {...field} />
                                             </FormControl>
-                                            <FormDescription>
-                                                This is your public display name.
-                                            </FormDescription>
-                                            <FormMessage />
+                                            <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="name"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input placeholder="Email address" {...field} />
+                                            </FormControl>
+                                            <FormMessage/>
+                                        </FormItem>
+                                    )}
+                                />
+                                <FormField
+                                    control={form.control}
+                                    name="name"
+                                    render={({field}) => (
+                                        <FormItem>
+                                            <FormControl>
+                                                <Input placeholder="Password" {...field} type='password'/>
+                                            </FormControl>
+                                            <FormMessage/>
                                         </FormItem>
                                     )}
                                 />
                                 <Button type="submit">Submit</Button>
                             </form>
                         </Form>
-                    </div>
-                    <div className='flex justify-center'>
-                        <Card className={cn("w-[380px]")}>
-                            <CardHeader>
-                                <CardTitle>Notifications</CardTitle>
-                                <CardDescription>You have 3 unread messages.</CardDescription>
-                            </CardHeader>
-                            <CardContent className="grid gap-4">
-                                <div className=" flex items-center space-x-4 rounded-md border p-4">
-                                    <BellRing />
-                                    <div className="flex-1 space-y-1">
-                                        <p className="text-sm font-medium leading-none">
-                                            Push Notifications
-                                        </p>
-                                        <p className="text-sm text-muted-foreground">
-                                            Send notifications to device.
-                                        </p>
-                                    </div>
-                                    <Switch />
-                                </div>
-                                <div>
-                                    {notifications.map((notification, index) => (
-                                        <div
-                                            key={index}
-                                            className="mb-4 grid grid-cols-[25px_1fr] items-start pb-4 last:mb-0 last:pb-0"
-                                        >
-                                            <span className="flex h-2 w-2 translate-y-1 rounded-full bg-sky-500" />
-                                            <div className="space-y-1">
-                                                <p className="text-sm font-medium leading-none">
-                                                    {notification.title}
-                                                </p>
-                                                <p className="text-sm text-muted-foreground">
-                                                    {notification.description}
-                                                </p>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div>
-                            </CardContent>
-                            <CardFooter>
-                                <Button className="w-full">
-                                    <Check className="mr-2 h-4 w-4" /> Mark all as read
-                                </Button>
-                            </CardFooter>
-                        </Card>
                     </div>
                 </div>
             </main>

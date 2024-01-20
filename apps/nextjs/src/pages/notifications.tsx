31ee84a8763a2d08529f694f371c8431358eb601
Chipzstar <chipzstar.dev@googlemail.com>
2024-01-20 14:28:37 +0000

    add missing pages

---
+++ apps/nextjs/src/pages/notifications.tsx
@@ -0,0 +1,39 @@
+import React, {ReactElement} from 'react';
+import {PATHS} from "~/utils";
+import {Button} from "@genus/ui/button";
+import AppLayout from "~/layout/AppLayout";
+import { Navbar, NavbarBrand } from '@nextui-org/react';
+import { useRouter } from 'next/router';
+import {SignedIn, useAuth } from '@clerk/nextjs';
+import Image from 'next/image';
+
+const Notifications = () => {
+    const router = useRouter()
+    const { signOut } = useAuth();
+    return (
+        <div className='page-container'>
+            <Navbar classNames={{
+                brand: 'w-full flex justify-center items-center',
+            }}>
+                <NavbarBrand role="button" onClick={() => router.push(PATHS.HOME)}>
+                    <Image src='/images/white-logo.svg' alt='genus-white' width={100} height={75}/>
+                    <div className='absolute right-4'>
+                        <SignedIn>
+                            <Button size="sm" onClick={(e) => signOut()}>Logout</Button>
+                        </SignedIn>
+                    </div>
+                </NavbarBrand>
+            </Navbar>
+            <div className='bg-white h-full p-6 sm:px-12 sm:pt-12'>
+            </div>
+        </div>
+    );
+};
+
+Notifications.getLayout = function getLayout(page: ReactElement) {
+    return (
+        <AppLayout>{page}</AppLayout>
+    )
+}
+
+export default Notifications;
--- packages/api/src/router/reaction.ts
+++ packages/api/src/router/reaction.ts
@@ -25,7 +25,6 @@ export const reactionRouter = createTRPCRouter({
                     }
                 });
             }
-            console.log(reaction);
             return reaction;
         } catch (err) {
             console.error(err);

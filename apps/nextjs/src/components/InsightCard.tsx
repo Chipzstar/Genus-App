57c7c7d10dd81c629255c46a729f0b804996bce3
Chipzstar <chipzstar.dev@googlemail.com>
2024-01-23 10:40:33 +0000

    optimize industry insights page for mobile

--- apps/nextjs/src/components/InsightCard.tsx
+++ apps/nextjs/src/components/InsightCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react';
+import React, {FC} from 'react';
 import Image from 'next/image';
 
 interface Props {
@@ -7,13 +7,13 @@ interface Props {
     image: string;
 }
 
-const InsightCard : FC<Props> = (props: Props) => {
-    const { id, title, image } = props;
+const InsightCard: FC<Props> = (props: Props) => {
+    const {id, title, image} = props;
     return (
-        <div className="flex items-center space-x-3 pb-6 text-black">
-            <Image src={image} alt={title} width={150} height={100}/>
-            <header className="sm:w-96">
-                <span className="text-sm sm:text-2xl font-bold whitespace-pre-wrap">{title}</span>
+        <div className="flex items-center space-x-3 sm:space-x-8 sm:pb-6 text-black">
+            <img src={image} alt={title} className="object-fit w-32 sm:w-40"/>
+            <header className="w-32 grow sm:w-96">
+                <span className="leading-tight sm:text-2xl font-bold whitespace-pre-wrap">{title}</span>
             </header>
         </div>
     );

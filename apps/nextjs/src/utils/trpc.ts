b97f321c62f97b72eb76aa0a5be9c8bf2819f07f
Chipzstar <chipzstar.dev@googlemail.com>
2024-02-12 07:15:11 +0000

    debug process.env

--- apps/nextjs/src/utils/trpc.ts
+++ apps/nextjs/src/utils/trpc.ts
@@ -1,33 +1,36 @@
 // src/utils/trpc.ts
-import { createTRPCNext } from '@trpc/next';
-import { httpBatchLink, loggerLink } from '@trpc/client';
-import { inferRouterInputs, inferRouterOutputs } from '@trpc/server';
-import type { AppRouter } from '@genus/api';
-import { transformer } from '@genus/api/transformer';
+import * as process from "process";
+import { httpBatchLink, loggerLink } from "@trpc/client";
+import { createTRPCNext } from "@trpc/next";
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+
+import type { AppRouter } from "@genus/api";
+import { transformer } from "@genus/api/transformer";
 
 const getBaseUrl = () => {
-    if (typeof window !== 'undefined') return ''; // browser should use relative url
-    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-    return `http://localhost:${process.env.PORT ?? 3001}`; // dev SSR should use localhost
+	console.log(process.env);
+	if (typeof window !== "undefined") return ""; // browser should use relative url
+	if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+	return `http://localhost:${process.env.PORT ?? 3001}`; // dev SSR should use localhost
 };
 
 export const trpc = createTRPCNext<AppRouter>({
-    config() {
-        return {
-            transformer,
-            links: [
-                loggerLink({
-                    enabled: opts =>
-                        process.env.NODE_ENV === 'development' ||
-                        (opts.direction === 'down' && opts.result instanceof Error)
-                }),
-                httpBatchLink({
-                    url: `${getBaseUrl()}/api/trpc`
-                })
-            ]
-        };
-    },
-    ssr: false
+	config() {
+		return {
+			transformer,
+			links: [
+				loggerLink({
+					enabled: opts =>
+						process.env.NODE_ENV === "development" ||
+						(opts.direction === "down" && opts.result instanceof Error)
+				}),
+				httpBatchLink({
+					url: `${getBaseUrl()}/api/trpc`
+				})
+			]
+		};
+	},
+	ssr: false
 });
 
 /**

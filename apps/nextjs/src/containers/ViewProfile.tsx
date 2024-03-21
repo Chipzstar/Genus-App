5c17cd1f3cd7e1c49e13ddf1bad9e5725099e66b
Chipzstar <chipzstar.dev@googlemail.com>
2024-03-21 16:07:29 +0000

    hide profile details for support admin user

--- apps/nextjs/src/containers/ViewProfile.tsx
+++ apps/nextjs/src/containers/ViewProfile.tsx
@@ -4,33 +4,44 @@ import { formatString } from "~/utils";
 import type { UserProfile } from "~/utils/types";
 import { Button } from "@genus/ui/button";
 import { MessagesSquare } from 'lucide-react'
+import * as process from "process";
 
 const ViewProfile = ({ profile }: { profile: UserProfile | undefined }) => {
+	if (profile?.clerkId === process.env.NEXT_PUBLIC_SUPPORT_ADMIN_USER) return (
+		<div className="flex flex-col grow justify-center items-center h-full">
+			<Button variant="ghost" size="lg" onClick={() => {
+				// @ts-expect-error chatwoot popup
+				window.$chatwoot.toggle("open");
+			}}>
+				<MessagesSquare size={25} className="mr-3"/> Chat with Support
+			</Button>
+		</div>
+	);
 	return (
 		<section className="flex grow flex-col bg-white p-6 h-full">
 			<div className="mb-4 flex flex-col">
-				<span className="text-xl font-bold text-primary">University</span>
-				<span className="text-base text-black md:text-lg">{formatString(profile?.university)}</span>
+				<span className="text-lg sm:text-xl font-bold text-primary">University</span>
+				<span className="text-sm sm:text-base text-black md:text-lg">{formatString(profile?.university)}</span>
 			</div>
 			{profile?.profileType !== "ADMIN" && <div className="mb-4 flex flex-col">
-				<span className="text-xl font-bold text-primary">Broad Degree Category</span>
-				<span className="text-base text-black md:text-lg">{formatString(profile?.broadDegreeCourse)}</span>
+				<span className="text-lg sm:text-xl font-bold text-primary">Broad Degree Category</span>
+				<span className="text-sm sm:text-base text-black md:text-lg">{formatString(profile?.broadDegreeCourse)}</span>
 			</div>}
 			<div className="mb-4 flex flex-col">
-				<span className="text-xl font-bold text-primary">Degree</span>
-				<span className="text-base text-black md:text-lg">{formatString(profile?.degreeName)}</span>
+				<span className="text-lg sm:text-xl font-bold text-primary">Degree</span>
+				<span className="text-sm sm:text-base text-black md:text-lg">{formatString(profile?.degreeName)}</span>
 			</div>
 			<div className="mb-4 flex flex-col">
-				<span className="text-xl font-bold text-primary">Completion Year</span>
-				<span className="text-base text-black md:text-lg">{profile?.completionYear}</span>
+				<span className="text-lg sm:text-xl font-bold text-primary">Completion Year</span>
+				<span className="text-sm sm:text-base text-black md:text-lg">{profile?.completionYear}</span>
 			</div>
 			<div className="mb-4 flex flex-col">
-				<span className="text-xl font-bold text-primary">Career Interests</span>
-				<span className="text-base text-black md:text-lg">
+				<span className="text-lg sm:text-xl font-bold text-primary">Career Interests</span>
+				<span className="text-sm sm:text-base text-black md:text-lg">
 					{profile?.careerInterests?.map(item => formatString(item.slug)).join(", ")}
 				</span>
 			</div>
-			<div className="flex flex-col grow justify-center items-center h-32">
+			<div className="flex flex-col grow justify-center items-center h-10 sm:h-32">
 				<Button variant="ghost" size="lg" onClick={() => {
 					// @ts-expect-error chatwoot popup
 					window.$chatwoot.toggle("open");

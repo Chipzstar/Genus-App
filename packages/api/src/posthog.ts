abbe65f2bd6f69a4046d873f4d2e259e4d3023e0
Chipzstar <chipzstar.dev@googlemail.com>
2024-09-09 11:40:19 +0100

    fix posthog-node client init

--- packages/api/src/posthog.ts
+++ packages/api/src/posthog.ts
@@ -4,7 +4,7 @@ const token = process.env.POSTHOG_API_TOKEN!;
 
 export const posthog = new PostHog(
 	token,
-	{ host: "https://eu.i.posthog.com", enable: process.env.NODE_ENV === "production" } // You can omit this line if using PostHog Cloud
+	{ host: "https://eu.i.posthog.com", disabled: process.env.NODE_ENV !== "production" } // You can omit this line if using PostHog Cloud
 );
 
-await posthog.shutdownAsync(); // TIP: On program exit, call shutdown to stop pending pollers and flush any remaining events
+await posthog.shutdown(); // TIP: On program exit, call shutdown to stop pending pollers and flush any remaining events
